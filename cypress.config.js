3bcc9f2f27fc13f978df33182172a7f097033758
FGGentile <francog@nimble.la>
2024-03-26 14:27:21 -0300

    v1

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: true,
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'results/test-results.xml',
-    outputs: true,
-    testCaseSwitchClassnameAndName: true
-  },
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
 
 
     baseUrl: "https://automationexercise.com/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
 
   },
